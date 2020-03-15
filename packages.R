074c1f768e46ca91e7bca93d8d1ce8b001931e38
Johan-rosa <Johan.rosaperez@gmail.com>
2020-03-14 21:59:15 -0400

    packages script

---
+++ packages.R
@@ -0,0 +1,12 @@
+# kaggle publication
+install.packages(c("broom", "ggridges", "ggthemes", "janitor", "lubridate", "tidyverse"))
+# maps RD
+install.packages(c("gridExtra", "kableExtra", "leaflet", "raster", "rgdal", "sf", "tmap", "viridis"))
+# maps with sf
+install.packages("ggrepel")
+# introduccion tidyverst
+install.packages(c("fable", "fabletools", "forecast", "tsibble"))
+# defunciones
+install.packages(c("gganimate", "patchwork"))
+# multiples modelos
+install.packages("sweep")
